3a91b4a60fe393606e48ba0aeb8c589bd5121f14
Robin Elvedi <trivialic@posteo.net>
2020-10-07 16:43:46 +0200

    init

---
+++ IERC20.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.24;
+
+/*
+ *  ERC20 Interface as specified on https://eips.ethereum.org/EIPS/eip-20
+ */
+interface IERC20 {
+    function name() external view returns (string);
+    function symbol() external view returns (string);
+    function decimals() external view returns (uint8);
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address owner) external view returns (uint256 balance);
+    function transfer(address to, uint256 value) external returns (bool success);
+    function transferFrom(address from, address to, uint256 value) external returns (bool success);
+    function approve(address spender, uint256 value) external returns (bool success);
+    function allowance(address owner, address spender) external view returns (uint256 remaining);
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
---
+++ KDSToken.sol
@@ -0,0 +1,83 @@
+pragma solidity ^0.4.24;
+
+import "./IERC20.sol";
+import "./libs/SafeMath.sol";
+
+contract KDSToken is IERC20 {
+    using SafeMath for uint256;
+
+    string public  name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    constructor() public {
+        name = "Kudos";
+        symbol = "KDS";
+        decimals = 0;
+
+        totalSupply = 100;
+
+        // Initially, all tokens belong to 0x6f72223547220640870a63e03C55Ecb55884a4DE
+        balances[0x6f72223547220640870a63e03C55Ecb55884a4DE] = totalSupply;
+        emit Transfer(address(0), 0x6f72223547220640870a63e03C55Ecb55884a4DE, totalSupply);
+    }
+
+    function name() public view returns (string) {
+        return name;
+    }
+
+    function symbol() public view returns (string) {
+        return symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return decimals;
+    }
+
+    // totalSupply is 0 because everything is first allocated to 0x6f72223547220640870a63e03C55Ecb55884a4DE
+    function totalSupply() public view returns (uint256) {
+        return totalSupply - balances[address(0)];
+    }
+
+    function balanceOf(address owner) public view returns (uint256 balance) {
+        return balances[owner];
+    }
+
+    function transfer(address to, uint256 tokens) public returns (bool success) {
+        require(tokens <= balances[msg.sender]);
+
+        balances[msg.sender] = balances[msg.sender].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 tokens) public returns (bool success) {
+        require(tokens <= balances[from]);
+        require(tokens <= allowed[from][msg.sender]);
+
+        balances[from] = balances[from].sub(tokens);
+        balances[to] = balances[to].add(tokens);
+
+        allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint256 tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256 remaining) {
+        return allowed[owner][spender];
+    }
+}
